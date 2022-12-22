081c92c501fec3038bff7aac60d7e005deba6c87
Kotaz <semechkagent@gmail.com>
2022-12-22 18:27:48 +0300

    Хз, что делать дальше

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from krpg.console import Console
-from krpg.scparse import parse, Section
+from krpg.scparse import Command, parse, Section
 
 
 
@@ -8,9 +8,15 @@ class Game:
     commands = []
     def __init__(self):
         self.console = Console()
+        
+        self.locations = {}
+        self.roads = {}
+        self.state = "RUN"
+        
         scenario = parse(open("scenario.krpg").read())
         self.init_scenario(scenario)
         self.main()
+        
         
 
     def run_command(self, command):
@@ -29,11 +35,56 @@ class Game:
             if section.head.command == "init":
                 self.execute(section.body)
             elif section.head.command == "map":
-                __import__('rich').print(section)
+                self.map_update(section)
+    
+    def map_update(self, section: Section):
+        for item in section.body:
+            if isinstance(item, Section):
+                if item.head.command == "location":
+                    self.locations[item.head.args[0]] = item
+                    continue
+            else:
+                if item.command == "link":
+                    self.roads[(item.args[0], item.args[1])] = True if len(item.args) == 3 else False
+                    continue
+            self.console.log(f"Unknown map command: {item}", 5)
+            exit(1)
+        print(self.locations)
+    
+    
+    def gen_panels(self):
+        return [
+            lambda: "Hello world"
+        ]
                 
     def main(self):
-        pass
+        while self.state == "RUN":
+            self.console.panels = self.gen_panels()
+            command = self.console.prompt()
+            self.console.panels = []
+            if not command.split():
+                continue
+            names = {i.command: i for i in self.manager.list_actions()}
+            if command in names:
+                self.handler.event(type="command", command=command, game=self)
+                names[command].callback(self)
+                self.clock.wait(names[command].time)
+            else:
+                self.console.print(
+                    f"[bold white]Команда {repr(command)} не найдена. Введите help для просмотра списка команд.[/]"
+                )
+                for category, actions in self.manager.actions().items():
+                    self.console.print(f"[bold white]{category}[/]: ", end="")
+                    self.console.print(
+                        ", ".join(f"[green]{action.command}[/]" for action in actions)
+                    )
 
 if __name__ == "__main__":
-    Game()
+    try:
+        Game()
+    except KeyboardInterrupt:
+        print("Bye!")
+    except Exception as e:
+        print('\033[0m')
+        raise e from None
     
