c0ff70ab542348f78005fc1bae35ae9e7185fa57
Kotaz <semechkagent@gmail.com>
2022-12-22 18:16:39 +0300

    Все еще неоднозначная логика

--- main.py
+++ main.py
@@ -1,48 +1,35 @@
 from krpg.console import Console
 from krpg.scparse import parse, Section
 
-# decorator that have callback and name: @command("name")
-def command(name: str):
-        def decorator(func):
-            func.name = name
-            return func
-        return decorator
-class Commands:
-    @command("print")
-    def print_command(game, args):
-            game.console.print(*args)
-    def __init__(self):
-        self.commands = {}
-        for name, func in self.__class__.__dict__.items():
-            if hasattr(func, "name"):
-                self.commands[func.name] = func
-                
+
+
+
 class Game:
     commands = []
     def __init__(self):
         self.console = Console()
-        self.commands = Commands()
-        
         scenario = parse(open("scenario.krpg").read())
         self.init_scenario(scenario)
         self.main()
         
 
     def run_command(self, command):
-        if command.command in self.commands.commands:
-            self.commands.commands[command.command](self, command.args)
+        if command.command == 'print':
+            self.console.print(*command.args)
         else:
-            self.console.print(f"Unknown command: {command.command}")
+            self.console.log(f"Unknown command: {command.command}", 5)
             exit(1)
 
-    def run_commands(self, commands):
+    def execute(self, commands):
         for command in commands:
             self.run_command(command)
     
     def init_scenario(self, scenario: list[Section]):
         for section in scenario:
             if section.head.command == "init":
-                self.run_commands(section.body)
+                self.execute(section.body)
+            elif section.head.command == "map":
+                __import__('rich').print(section)
                 
     def main(self):
         pass
