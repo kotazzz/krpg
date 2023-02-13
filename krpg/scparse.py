485ce596a92802edb0802613537b044fb9c75473
Kotaz <semechkagent@gmail.com>
2023-02-13 20:29:30 +0300

    Что вообще происходит?

--- krpg/scparse.py
+++ krpg/scparse.py
@@ -54,67 +54,28 @@ class Section:
         if isinstance(self.head, str):
             self.head = Command(self.head)
 
-    def get_section(self, name):
-        for item in self.body:
-            if isinstance(item, Section) and item.head.command == name:
-                return item
-
-    def get_sections(self, name):
-        for item in self.body:
-            if isinstance(item, Section) and item.head.command == name:
-                yield item
-
-    def get_command(self, name):
-        for item in self.body:
-            if isinstance(item, Command) and item.command == name:
-                return item
-
-    def get_commands(self, name):
-        for item in self.body:
-            if isinstance(item, Command) and item.command == name:
-                yield item
-
     def get_items(self, name):
         for item in self.body:
-            if isinstance(item, (Section, Command)):
-                yield item
-
-    def get_all_commands(self):
-        for item in self.body:
-            if isinstance(item, Command):
-                yield item
-
-    def get_all_sections(self):
-        for item in self.body:
             if isinstance(item, Section):
-                yield item
+                if item.head.command == name:
+                    yield item
+            elif isinstance(item, Command):
+                if item.command == name:
+                    yield item
 
-    def __getitem__(self, key) -> list[Section | Command] | Section | Command:
-        if isinstance(key, slice):
-            # 1 - name
-            # 2 - one, all (default)
-            # 3 - commands, sections, all (default)
-            name, mode, type = key.start, key.stop or "all", key.step or "all"
-            if name:
-                funcs = {
-                    ("command", "one"): self.get_command,
-                    ("command", "all"): lambda x: list(self.get_commands(x)),
-                    ("section", "one"): self.get_section,
-                    ("section", "all"): lambda x: list(self.get_sections(x)),
-                    ("all", "one"): lambda x: self.get_items(x)[0],
-                    ("all", "all"): lambda x: list(self.get_items(x)),
-                }
-                return funcs[type, mode](name)
-            else:
-                # return all
-                funcs = {
-                    "command": lambda: self.get_all_commands(),
-                    "section": lambda: self.get_all_sections(),
-                    "all": lambda: self.get_items(),
-                }
-                return list(funcs[type]())
+    @property
+    def sections(self):
+        return [x for x in self.body if isinstance(x, Section)]
 
-        return list(self.get_items(key))
+    @property
+    def commands(self):
+        return [x for x in self.body if isinstance(x, Command)]
+
+    def __getattribute__(self, __name: str):
+        if __name in ("head", "body", "sections", "commands", "get_items", "save", "load", "__post_init__"):
+            return super().__getattribute__(__name)
+        else:
+            return list(self.get_items(__name))
 
 
 def parse(text):
