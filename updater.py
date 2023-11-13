7686fb84400bb943d7068e0cbcd211a2b7efeacd
Kotaz <semechkagent@gmail.com>
2023-11-13 21:29:55 +0300

    Фикс проблем

--- updater.py
+++ updater.py
@@ -1,6 +1,6 @@
 import requests
 import os
-import hashlib
+import zlib
 import glob
 link = "https://raw.githubusercontent.com/kotazzz/krpg/master/"
 files = [
@@ -16,8 +16,7 @@ def local_hashes():
         for j in glob.glob(i):
             # get hash
             with open(j, "rb") as f:
-                # hashes.append((j, hashlib.md5(f.read()).hexdigest()))
-                hashes[j] = hashlib.md5(f.read()).hexdigest()
+                hashes[j] = zlib.crc32(f.read())
     return hashes
 
 def get_hashes():
@@ -31,27 +30,38 @@ def update():
     # get hashes from both sides
     local = local_hashes()
     remote = get_hashes()
+    
     queue = []
-    for i in local:
-        if i not in remote:
-            print(f"New file: {i}")
-            queue.append(i)
-        if local[i] != remote[i]:
-            print(f"Updated: {i}")
-            queue.append(i)
     for i in remote:
         if i not in local:
+            print(f"New file: {i}")
+            queue.append(i)
+        elif local[i] != remote[i]:
+            print(f"Updated: {i}")
+            print(local[i], remote[i])
+            queue.append(i)
+    for i in local:
+        if i not in remote:
             print(f"Deleted: {i}")
             os.remove(i)
     for i in queue:
         print(f"Downloading: {i}")
         # download
-        url = link + i
+        url = (link + i).replace("\\", "/")
         r = requests.get(url)
         if r.status_code != 200:
             raise Exception(f"Can't download {url}: {r.status_code}, {r.reason}")
         # create folders
-        os.makedirs(os.path.dirname(i), exist_ok=True)
+        try:
+            os.makedirs(os.path.dirname(i), exist_ok=True)
+        except FileNotFoundError:
+            print("Skipping folder creation")
         # save
         with open(i, "wb") as f:
-            f.write(r.read())
+            f.write(r.content)
+
+def main():
+    update()
+
+if __name__ == "__main__":
+    main()
