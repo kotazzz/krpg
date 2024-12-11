d286b1236f6846d70aa40bf5869405ade04aaa70
Kotaz <semechkagent@gmail.com>
2024-12-11 21:17:10 +0300

    Починина аннотация для commands

--- krpg/commands.py
+++ krpg/commands.py
@@ -10,22 +10,27 @@ type CommandCallback = Callable[..., Generator[Event, Any, Any]]
 
 
 @attr.s(auto_attribs=True)
-class Command:
-    callback: CommandCallback
+class Command[**P, R]:
+    callback: Callable[P, R]
     args: tuple[Any, ...] = attr.ib(factory=tuple)
     kwargs: dict[str, Any] = attr.ib(factory=dict)
 
-    def __call__(self, *args: Any, **kwargs: Any) -> Any:
+
+    def __call__(self, *args: P.args, **kwargs: P.kwargs) -> Command[P, R]:
         self.args = args
         self.kwargs = kwargs
         return self
+    
+def command[**P, R](callback: Callable[P, R]) -> Command[P, R]:
+    return Command(callback=callback)
+
 
 
 class CommandManager:
     def __init__(self, event_handler: EventHandler) -> None:
         self.event_handler = event_handler
 
-    def execute(self, command: Command) -> Any | None:
+    def execute(self, command: Command[[int], Any]) -> Any | None:
         a, k = command.args, command.kwargs
         gen = command.callback(*a, **k)
         for event in gen:
@@ -37,5 +42,4 @@ class CommandManager:
         return None
 
 
-def command(callback: CommandCallback) -> Command:
-    return Command(callback=callback)
+
